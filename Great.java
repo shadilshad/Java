d6eb9068eae4c843fdbd25b8c4859bb414a6b0f3
SHADIL VV <shadilshad508@gmail.com>
2025-10-21 22:27:47 +0530

    Initial commit

--- Great.java
+++ Great.java
@@ -1,13 +1,23 @@
-public class Great{
-    public static void main(String[] args){
-        System.out.println("here we go");
-
-        int a=10;
+public class Great
+    {
+        public static void main(String[] args)
+    {
+          System.out.println("here we go");
+         int a=10;
         int b=50;
         int c=a+b;
         System.out.println("a+b is: "+ c);
-
+        boolean len=false;
+        System.out.println(len);
+        if (len==false)
+    {
+            System.out.println("idk rabbe");
     }
-}
+        else
+    {
+            System.out.println("fak");
+    }
+    }
+    }
 
 
