5e00333d5c0a450e52419db54eb52d2685f8c243
SHADIL VV <shadilshad508@gmail.com>
2025-10-21 22:21:49 +0530

    Initial commit

--- Great.java
+++ Great.java
@@ -1,6 +1,12 @@
 public class Great{
     public static void main(String[] args){
         System.out.println("here we go");
+
+        int a=10;
+        int b=50;
+        int c=a+b;
+        System.out.println("a+b is: "+ c);
+
     }
 }
 
