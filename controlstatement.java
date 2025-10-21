45f5c59754a54ce31b7c081da14db95f9329105d
SHADIL VV <shadilshad508@gmail.com>
2025-10-21 22:49:48 +0530

    Initial commit

--- controlstatement.java
+++ controlstatement.java
@@ -10,6 +10,26 @@ public class controlstatement {
             System.out.println("fak");
     }
 
+    String name="shadil";
+    int mark=80;
+    String grade="";
+    if(mark>=90){
+grade=("A");
+    }
+    else if(mark>=80){
+grade=("B");
+    }
+    else if(mark>=70){
+grade=("C");
+    }   
+    else if(mark>=60){
+grade=("D");
+    }
+    else{
+grade=("FAIL");
 
+    }
+        System.out.println("The Student Named "+name + " Scored a "+ mark+" in the examination which has the grade of "+grade);
+ 
     }
 }
