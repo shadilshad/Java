37c36681888e3715d300f89002826709f83265f3
SHADIL VV <shadilshad508@gmail.com>
2025-10-22 20:02:01 +0530

    practice of js

--- print.js
+++ print.js
@@ -1,2 +1,10 @@
 console.log("hello world");
-alert("hi, there!");
+window.alert("hi, there!");
+
+let cars = ["bmw","audi","honda"];
+let x=10;
+let y=15;
+let z= x+y;
+document.getElementById("hello").innerHTML= cars + " and " + z ;
+
+console.log(`the value of given items like ${cars} cost a lot`);
