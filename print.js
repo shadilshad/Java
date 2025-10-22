691c8d4e46d5c37cf435b89ec926a50a74c13c89
SHADIL VV <shadilshad508@gmail.com>
2025-10-22 20:40:11 +0530

    practice of js

--- print.js
+++ print.js
@@ -1,5 +1,5 @@
 console.log("hello world");
-window.alert("hi, there!");
+window.alert(`Welcome Home`);
 
 let cars = ["bmw","audi","honda"];
 let x=10;
@@ -12,3 +12,7 @@ console.log(`the value of given items like ${cars} cost a lot`);
 let age = 22;
 age ++;
 console.log(age);
+
+let username = window.prompt("whats your name?");
+console.log(username)
+document.getElementById("user").innerHTML= username ;
